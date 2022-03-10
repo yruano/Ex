7397a5c757d43ce8060563959a17de3b906ebd00
Miano <51043000+yruano@users.noreply.github.com>
2022-03-10 03:46:12 +0000

    destructring

--- src/main.rs
+++ src/main.rs
@@ -388,3 +388,43 @@
 //     } 
 //   }
 // }
+
+
+// destructuring
+
+struct Person {
+    name: String,
+    real_name: String,
+    height: u8,
+    happiness: bool
+}
+
+#[derive(Debug)]
+struct Person2 {
+    name: String,
+    height: u8
+}
+
+impl Person2 {
+    fn from_person(input: Person) -> Self {
+        let Person {name, height, .. } = input;
+
+        Self {
+            name,
+            height
+        }
+    }
+}
+
+fn main() {
+    let papa_doc = Person {
+        name: "Papa Doc".to_string(),
+        real_name: "Clarence".to_string(),
+        height: 170,
+        happiness: false,
+    };
+    
+    let person2 = Person2::from_person(papa_doc);
+
+    println!("Person2 type is : {:?}", person2);
+}
